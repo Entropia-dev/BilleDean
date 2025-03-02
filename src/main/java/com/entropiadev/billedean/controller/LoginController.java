7fe90a6776fb03ae594dc8acf920744dfdb751cd
Entropia-Dev <cuenta.fede.parrotta@gmail.com>
2025-03-02 17:20:46 -0300

    Prueba del correcto funcionamiento en git

--- src/main/java/com/entropiadev/billedean/controller/LoginController.java
+++ src/main/java/com/entropiadev/billedean/controller/LoginController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-
+    //comentario desde intelij
+    // HOLA
     @GetMapping("/login")
     public String showLoginPage() {
         return "Login/Login"; // Este es el nombre de la plantilla, sin la extensión .html
